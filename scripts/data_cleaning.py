bda7b7744eb2948534d69ec3c28d0fa8503e507e
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-08-24 01:37:11 +0300

    drop columns with many null values

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -29,3 +29,21 @@ class DataCleaning:
         """
         self.df.drop(columns, axis=1, inplace=True)
         return self.df
+
+    def drop_column_with_many_null_values(self):
+        '''
+        Return List of Columns which contain more than 30% of null values
+        '''
+        df_size = self.df.shape[0]
+        
+        columns_list = self.df.columns
+        many_null_columns = []
+        
+        for column in columns_list:
+            null_per_column = self.df[column].isnull().sum()
+            percentage = round((null_per_column / df_size) * 100 , 2)
+            
+            if(percentage > 30):
+                many_null_columns.append(column)
+        
+        return many_null_columns
