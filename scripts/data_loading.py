90e142d28b3b2eacb52450b66ef1ea7d3c463e3e
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-08-23 18:05:35 +0300

    add DataLoader class

--- scripts/data_loading.py
+++ scripts/data_loading.py
@@ -1,51 +1,55 @@
 import pandas as pd;
 
-# list of possible na_values
-na_values = ["n/a", "na", 'none', "-", "--", None, '?']
+class DataLoader():
+    def __init__(self, df):
+        self.df = df.copy()
+        
+    # list of possible na_values
+    na_values = ["n/a", "na", 'none', "-", "--", None, '?']
 
-def load_data_from_csv(filepath: str, na_values: list = []) -> pd.DataFrame:
-    """
-        A  function which returns a dataframe from a specified .csv file
-        Parameters
-        ----------
-        filepath:
-            Type: str
-        na_values:
-            Type: list
-            Default value = []
+    def load_data_from_csv(filepath: str, na_values: list = []) -> pd.DataFrame:
+        """
+            A  function which returns a dataframe from a specified .csv file
+            Parameters
+            ----------
+            filepath:
+                Type: str
+            na_values:
+                Type: list
+                Default value = []
 
-        Returns
-        -------
-        pd.DataFrame
-    """
-    try:
-        na_values.extend(na_values)
-        df = pd.read_csv(filepath, na_values=na_values)
+            Returns
+            -------
+            pd.DataFrame
+        """
+        try:
+            na_values.extend(na_values)
+            df = pd.read_csv(filepath, na_values=na_values)
 
-        return df
-    except:
-        print("Error:\n\tCould not find specified .csv file")
+            return df
+        except:
+            print("Error:\n\tCould not find specified .csv file")
 
 
-def load_data_from_xsl(filepath: str, na_values: list = []) -> pd.DataFrame:
-    """
-        A  function which returns a dataframe from a specified .xslx file
-        Parameters
-        ----------
-        filepath:
-            Type: str
-        na_values:
-            Type: list
-            Default value = []
+    def load_data_from_xsl(filepath: str, na_values: list = []) -> pd.DataFrame:
+        """
+            A  function which returns a dataframe from a specified .xslx file
+            Parameters
+            ----------
+            filepath:
+                Type: str
+            na_values:
+                Type: list
+                Default value = []
 
-        Returns
-        -------
-        pd.DataFrame
-    """
-    try:
-        na_values.extend(na_values)
-        df = pd.read_excel(filepath, na_values=na_values, engine='openpyxl')
+            Returns
+            -------
+            pd.DataFrame
+        """
+        try:
+            na_values.extend(na_values)
+            df = pd.read_excel(filepath, na_values=na_values, engine='openpyxl')
 
-        return df
-    except:
-        print("Error:\n\tCould not find specified .xslx file")
+            return df
+        except:
+            print("Error:\n\tCould not find specified .xslx file")
