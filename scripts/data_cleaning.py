16206440637b1af3a02cadff86da9d6cb18cec84
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-08-23 20:22:12 +0300

    add remove_unwanted_columns function

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -15,4 +15,17 @@ class DataCleaning:
         """
         self.df = df
 
-    
+    def remove_unwanted_columns(self, columns: list) -> pd.DataFrame:
+        """
+        Returns a DataFrame where the specified columns in the list are removed
+        Parameters
+        ----------
+        columns:
+            Type: list
+
+        Returns
+        -------
+        pd.DataFrame
+        """
+        self.df.drop(columns, axis=1, inplace=True)
+        return self.df
