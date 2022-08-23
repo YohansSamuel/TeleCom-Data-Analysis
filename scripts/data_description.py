9091f29161186ee05f0766f01780a1a8f549dd46
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-08-23 18:51:44 +0300

    add df_skewness function

--- scripts/data_description.py
+++ scripts/data_description.py
@@ -6,20 +6,26 @@ class DataDescription():
 
     def df_columns_list(self):
         '''
-        Return Column list of the Dataframe
+        Return Column list of the DataFrame
         '''
         return self.df.columns.to_list()
 
     def df_detail_info(self):
         '''
-        Display the detail of the DataFrame information
+        Display the detail of the DataFrame
         '''
 
         print(self.df.info())
 
     def df_null_counts(self):
         '''
-        Display Null Counts of each column
+        Display null counts of each column
         '''
 
         print(self.df.isnull().sum())
+
+    def df_skewness(self):
+        '''
+        Display skewness of each column
+        '''
+        print(self.df.skew())
