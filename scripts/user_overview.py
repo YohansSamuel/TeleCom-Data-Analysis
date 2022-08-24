b1abadd363bcd4c77967fc77100e56fc4116a793
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-08-24 20:42:30 +0300

    improve code readability

--- scripts/user_overview.py
+++ scripts/user_overview.py
@@ -1,30 +1,25 @@
-from lib2to3.pgen2.pgen import DFAState
-from turtle import pd
-
-
 import pandas as pd
 
 class UserOverview():
-    def __init__(self) -> None:
+    def __init__(self,df) -> None:
         self.df = df 
     
-# get the top handsets
-def get_top_handsets(df,num):
-    top_handsets = df['Handset Type'].value_counts().head(num)
-    return top_handsets
+    # get the top handsets
+    def get_top_handsets(self,num):
+        top_handsets = self.df['Handset Type'].value_counts().head(num)
+        return top_handsets
 
-# get the top manufacturers
-def get_top_manufacturers(df,num):
-    top_manufacturers = df['Handset Manufacturer'].value_counts().head(num)
-    return top_manufacturers
+    # get the top manufacturers
+    def get_top_manufacturers(self,num):
+        top_manufacturers = self.df['Handset Manufacturer'].value_counts().head(num)
+        return top_manufacturers
+    # get the top handsets per top manufacturers
+    def get_top_handset_by_top_manufacturer(self,handset_num,manufacturer_num):
+            top_3_manufacturers = self.get_top_manufacturers(manufacturer_num)
 
-def get_top_handset_by_top_manufacturer(self):
-        top_3_manufacturers = self.get_top_manufacturers(3)
+            manufacturers = self.df.groupby("Handset Manufacturer")
 
-        manufacturers = self.df.groupby("Handset Manufacturer")
-
-        for manufacturer in top_3_manufacturers.index:
-            result = manufacturers.get_group(manufacturer).groupby("Handset Type")['MSISDN/Number'].nunique().nlargest(5)
-            print(f"****{ manufacturer} ****")
-            print(result)
-            print() 
+            for manufacturer in top_3_manufacturers.index:
+                result = manufacturers.get_group(manufacturer).groupby("Handset Type")['MSISDN/Number'].nunique().nlargest(handset_num)
+                print(f"****{ manufacturer} ****")
+                print(result)
