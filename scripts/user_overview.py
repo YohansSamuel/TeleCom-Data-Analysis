7ac94d76706f23e5f42487da335744f4fd58efe5
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-08-24 19:52:01 +0300

    add top_handset_by_top_manufacture method

--- scripts/user_overview.py
+++ scripts/user_overview.py
@@ -17,3 +17,14 @@ def get_top_handsets(df,num):
 def get_top_manufacturers(df,num):
     top_manufacturers = df['Handset Manufacturer'].value_counts().head(num)
     return top_manufacturers
+
+def get_top_handset_by_top_manufacturer(self):
+        top_3_manufacturers = self.get_top_manufacturers(3)
+
+        manufacturers = self.df.groupby("Handset Manufacturer")
+
+        for manufacturer in top_3_manufacturers.index:
+            result = manufacturers.get_group(manufacturer).groupby("Handset Type")['MSISDN/Number'].nunique().nlargest(5)
+            print(f"****{ manufacturer} ****")
+            print(result)
+            print() 
