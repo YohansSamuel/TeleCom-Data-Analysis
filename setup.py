52c79cd23bca45f539edf781ff304c51bd14ae5e
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-08-28 18:08:34 +0300

    update setup .py file

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="A Repository structure for all your project",
     install_requires=requirements,
