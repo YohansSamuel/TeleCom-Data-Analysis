0aa2172b999b2d1781a55445b344e301e7d878e9
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-08-28 04:57:06 +0300

    add pie chart

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,13 +1,15 @@
+from cProfile import label
 from tkinter import PAGES
 import numpy as np
 import pandas as pd
 import streamlit as st
-
+import plotly.graph_objects as go
 # 
 import sys
 sys.path.insert(0,'./scripts/')
 from user_overview import UserOverview
 import user_engagement_dashboard
+from data_plots import plot_hist,plot_mult_hist
 
 #
 import seaborn as sns
@@ -41,10 +43,41 @@ def user_overview_analysis():
 
     
     # display the top_handsets result in bar graph
-    
+    # plt.figure(figsize=(10,5))
+    # sns.barplot(x=top_3_manufacturers.index, y=top_3_manufacturers.values)
+    # plt.title('Top 3 Handset Manufacturers', size=14, fontweight="bold")
+    # plt.xlabel('Handset Manufacturers', size=13, fontweight="bold") 
+    # plt.ylabel('Number of Users', size=13, fontweight="bold")
+    # plt.show()
+
+    fig = go.Figure(go.Pie(labels = top_3_manufacturers.index,
+    values = top_3_manufacturers.values
+    ))
+    st.header("Top 3 Handset Manufacturers")
+    st.plotly_chart(fig)
 # 
 def user_engagement_analysis():
     st.title("User Engagement Analysis")
+    df = pd.read_csv('./data/user_engagement.csv',index_col=0)
+    user_engagement_df = df[['Cluster','Session Frequency','Duration','Total Data Usage']]
+    user_engagement_df = user_engagement_df.groupby('Cluster').agg({'Session Frequency':'count',
+    'Duration':'sum','Total Data Usage':'sum'})
+
+    col = st.sidebar.selectbox(
+        "Select top 10 from", (["Session Frequency", "Duration", "Total Data Usage"]))
+    if col == "Sessions_Frequency":
+        sessions = df.nlargest(10, "Session Frequency")['Session Frequency']
+        return plot_hist(sessions)
+    elif col == "Duration":
+        duration = df.nlargest(10, "Duration")['Duration']
+
+        return plot_mult_hist([duration], 1, 1, "User Engagement Duration", ['Duration (sec)'])
+
+    else:
+        total_data_volume = df.nlargest(
+            10, "Total Data Usage")['Total Data Usage']
+        
+        return plot_mult_hist([total_data_volume], 1, 1, "User Engagement Total Data Usage", ['Total Data Usage (kbps)'])
 
 # 
 def user_experience_analysis():
